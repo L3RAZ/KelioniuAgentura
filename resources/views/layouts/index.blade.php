ab388d4db051be849f860e5ea2615f77806be33c
Emilija <your_email@youremail.comemilija.raciute@gmail.com>
2017-11-24 12:08:02 +0200

    layout

---
+++ resources/views/layouts/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.layout')
+
+@section('content')
+<div>
+    <h2>Kelionių pasiūlymai</h2>
+</div>
+@endsection
---
+++ resources/views/layouts/layout.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Kelionių Agentūra</title>
+	<link rel="stylesheet" type="text/css" href="<?php echo asset('../css/style.css')?>" type="text/css">
+	<link href="https://fonts.googleapis.com/css?family=Hind+Vadodara:500" rel="stylesheet">
+</head>
+<body>
+<header>
+	<div class="headerImg">
+		<div class="headerTitle">
+			<h1>Kelionių agentūra</h1>
+		</div>		
+	</div>
+</header>
+<div class="content">
+@yield('content')
+</div>
+<footer>
+	<p>2017 &copy; Yoga Cats</p>
+</footer>
+</body>
+</html>
