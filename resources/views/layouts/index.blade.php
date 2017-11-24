a7bbfd05f041efcfb97ed70d74ce5ee64b5af90a
Emilija <your_email@youremail.comemilija.raciute@gmail.com>
2017-11-24 12:59:27 +0200

    -

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -3,4 +3,5 @@
 @section('content')
 <div class="col-6 col-sm-6 col-md-6 col-xs-6"><input type="button" name="prisijungti" value="Prisijungti" class="button"></div>
 <div class="col-6 col-sm-6 col-md-6 col-xs-6"><input type="button" name="registruotis" value="Registruotis" class="button"></div>
+<h2>Kelionių pasiulymai</h2>
 @endsection
