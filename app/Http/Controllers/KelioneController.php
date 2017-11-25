10ce0818422c8e8b9e6df939b12f8101fe52904e
Emilija <your_email@youremail.comemilija.raciute@gmail.com>
2017-11-25 08:37:26 +0200

    viesbuciai

--- app/Http/Controllers/KelioneController.php
+++ app/Http/Controllers/KelioneController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Kelione;
 use App\Kelioniu_datos;
+use App\Viesbuciai_keliones;
 use DB;
 
 class KelioneController extends Controller
@@ -21,8 +22,8 @@ class KelioneController extends Controller
 
     public function show($id)
     {
-        $kelione = Kelione::select('keliones.id', DB::raw('miestas.pavadinimas as miesto_pav'), 
-                                    'valstybe', 'kaina', 'transporto_priemones', 'aprasymas', DB::raw('keliones_tipas.tipas as kel_tipas'))
+        $kelione = Kelione::select('keliones.id', DB::raw('miestas.pavadinimas as miesto_pav'), 'valstybe', 'kaina', 
+                    'transporto_priemones', 'aprasymas', DB::raw('keliones_tipas.tipas as kel_tipas'))
         ->join('miestas', 'miestas.kodas', '=', 'keliones.miesto_kodas')
         ->join('keliones_tipas', 'keliones_tipas.id', '=', 'keliones.tipas')
         ->where('keliones.id', '=', $id)
@@ -31,8 +32,15 @@ class KelioneController extends Controller
         $datos = Kelioniu_datos::select('id', 'keliones_nr', 'isvykimo_data', 'grizimo_data', 'laisvu_vietu_sk')
         ->where('keliones_nr', '=', $id)
         ->get();
+        
+        $viesbuciai = Viesbuciai_keliones::select('viesbucio_id', 
+                        DB::raw('viesbuciai.pavadinimas as pavadinimas'), DB::raw('viesbuciai.reitingas as reitingas'),
+                        DB::raw('viesbuciai.paros_kaina as paros_kaina'), DB::raw('viesbuciai.adresas as adresas'),
+                        DB::raw('viesbuciai.telefono_nr as tel_nr'))
+        ->join('viesbuciai', 'viesbuciai.id', '=', 'viesbuciai_keliones.viesbucio_id')
+        ->where('keliones_nr', '=', $id)
+        ->get();
 
-
-        return view('layouts.kelione', compact('kelione'), compact('datos'));
+        return view('layouts.kelione', compact('kelione', 'datos', 'viesbuciai'));//, compact('datos'), compact('viesbuciai'));
     }
 }
--- resources/views/layouts/kelione.blade.php
+++ resources/views/layouts/kelione.blade.php
@@ -57,6 +57,35 @@
     
 </div>
 @endforeach
+<div class="row"><p>Galimi viešbučiai:</p></div>
+@foreach($viesbuciai as $viesbutis)
+<div class="row">
+    
+        <table>
+            <tr>
+                <td>Pavadinimas: </td>
+                <td>{{ $viesbutis->pavadinimas}}</td>
+            </tr>
+            <tr>
+                <td>Reitingas: </td>
+                <td>{{ $viesbutis->reitingas}}</td>
+            </tr>
+            <tr>
+                <td>Paros kaina: </td>
+                <td>{{ $viesbutis->paros_kaina}}</td>
+            </tr> 
+            <tr>
+                <td>Adresas: </td>
+                <td>{{ $viesbutis->adresas}}</td>
+            </tr> 
+            <tr>
+                <td>Telefono nr.: </td>
+                <td>{{ $viesbutis->tel_nr}}</td>
+            </tr> 
+        </table>
+    
+</div>
+@endforeach
 <div class="row">
     <!-- sita paskui paslepti nuo neprisijungusiu vartotoju-->
     <div class="col-4 col-sm-4 col-md-4 col-xs-4 col-md-offset-4"><input type="button" name="uzsakyti" value="Užsakyti" class="button"></div>
