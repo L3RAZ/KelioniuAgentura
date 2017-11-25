0c10abcb90c78638185526d116882c0939b1f7bf
Misszukey <zaidimamsjot@gmail.com>
2017-11-25 17:53:45 +0200

    stilius

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -7,12 +7,13 @@
     <div class="col-6 col-sm-6 col-md-6 col-xs-6" style="width: 50%;"><input type="button" name="registruotis" value="Registruotis" class="button"></div>
 </div>
 <h2 class="col-12 col-sm-12 col-md-12 col-xs-12">Kelionių pasiūlymai</h2>
+
 <div class="row">
     <table id="kelionesTable">
             @foreach($keliones as $kelione)
                 <tr class="tableRow">
-                    <td style="width: 75%">{{$kelione['valstybe']}}      {{$kelione['miestas']}}     {{$kelione['kaina']}} &euro;</td>
-                    <td><a href="{{ url('/keliones/'.$kelione['id']) }}" onClick="KelioneController::show($kelione['id'])" style="width: 25%;">plačiau</a>
+                    <td style="width: 75%">{{$kelione['valstybe']}}  {{$kelione['miestas']}}     {{$kelione['kaina']}} &euro;</td>
+                    <td><a href="{{ url('/keliones/'.$kelione['id']) }}" onClick="KelioneController::show($kelione['id'])" style="width: 25%;"> plačiau</a>
                 </tr>
             @endforeach
     </table>
